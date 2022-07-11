acd70817d15efacd19e601da398145c7e00d3e23
zarnab777 <zarnab.bushra777@gmail.com>
2022-07-11 13:19:14 +0500

    Day4 Completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,13 +15,46 @@ class Loginpage extends StatelessWidget {
           "assets/images/login_image1.png",
           fit: BoxFit.cover,
         ),
+        SizedBox(
+          height: 20.0,
+        ),
         Text(
           "Welcome to the Login Page",
           style: TextStyle(
-            fontSize: 35,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
-        )
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 30.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter UserName ", labelText: "UserName"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password ", labelText: "Password"),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        ElevatedButton(
+            child: Text("Login"),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+            ),
+            onPressed: () {
+              print("Login Successful");
+            })
       ],
     ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_001/home_page.dart';
 import 'package:flutter_application_001/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 //import 'package:flutter_application_1/pages/home_page.dart';
 //import 'package:flutter_application_1/pages/login_page.dart';
 
@@ -20,6 +21,8 @@ class MyApp extends StatelessWidget {
         themeMode: ThemeMode.light,
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          // primaryTextTheme: GoogleFonts.latoTextTheme()
         ),
         darkTheme: ThemeData(
           // brightness: Brightness.dark,
