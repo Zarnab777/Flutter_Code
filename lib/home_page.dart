440a67a4757ea1fb134880f1a2e272919302951e
zarnab777 <zarnab.bushra777@gmail.com>
2022-07-14 17:34:18 +0500

    Day 11 Completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_001/models/catalog.dart';
 import 'package:flutter_application_001/widgets/drawer.dart';
+import 'package:flutter_application_001/widgets/item_widget.dart';
+import '';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(25, (index) => CatalogModel.items[0]);
+
     int days = 30;
     String name = "For Project";
     return Scaffold(
@@ -13,12 +18,17 @@ class Homepage extends StatelessWidget {
         title: Text("Catalog App",
             style: TextStyle(
               color: Colors.black,
-              
             )),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days of Flutter $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
