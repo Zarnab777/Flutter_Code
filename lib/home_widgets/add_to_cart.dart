93d3266774fc036a36ba35e6b114da9d381e9f41
zarnab777 <zarnab.bushra777@gmail.com>
2022-07-21 19:13:40 +0500

    Day 27 Completed

--- lib/home_widgets/add_to_cart.dart
+++ lib/home_widgets/add_to_cart.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_001/core/store.dart';
 import 'package:flutter_application_001/models/cart.dart';
 import 'package:flutter_application_001/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
-   AddToCart({
+  AddToCart({
     Key? key,
     required this.catalog,
   }) : super(key: key);
 
-  final _cart = CartModel();
-
   @override
   Widget build(BuildContext context) {
+    VxState.listen(context, to: [AddMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+
     bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
-
-          //setState(() {});
+          AddMutation(catalog);
         }
       },
       style: ButtonStyle(
