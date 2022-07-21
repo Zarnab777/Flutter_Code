a484be30a05df7cc5e016200e27dc45a8df58c6b
zarnab777 <zarnab.bushra777@gmail.com>
2022-07-21 11:32:32 +0500

    Day 21 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         theme: MyTheme.LightTheme(context),
         darkTheme: MyTheme.DarkTheme(context),
         debugShowCheckedModeBanner: false,
-        initialRoute: Myroutes.loginRoute,
+        initialRoute: Myroutes.homeRoute,
         routes: {
           "/": (context) => Loginpage(),
           Myroutes.loginRoute: (context) => Loginpage(),
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_001/widgets/themes.dart';
+//import 'package:flutter_application_001/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: context.canvasColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
